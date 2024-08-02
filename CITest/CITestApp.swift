818e2fc95912fa929a6b707524279b4b71bd6d4f
naoki.takehisa <naoki.takehisa@init-inc.com>
2024-08-02 10:44:43 +0900

    first commit

---
+++ CITest/CITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestApp.swift
+//  CITest
+//
+//  Created by 武久 なおき on 2024/08/02.
+//
+
+import SwiftUI
+
+@main
+struct CITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CITest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CITest
+//
+//  Created by 武久 なおき on 2024/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
